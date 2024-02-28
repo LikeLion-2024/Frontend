aff5e96b610a89f452040314f1a22bc138fff222
wonjeongjeong <wj3714@naver.com>
2024-02-28 15:26:45 +0900

    file add & delete

--- js/homework.js
+++ js/homework.js
@@ -1,9 +1,26 @@
 function handleFileSelection() {
-    var selectedFile = document.getElementById('fileInput').files[0];
-    if (selectedFile) {
-      document.getElementById('file_name').innerText = selectedFile.name;
-    } else {
-      document.getElementById('file_name').innerText = '파일이 선택되지 않았습니다.';
+    var selectedFiles = document.getElementById('fileInput').files;
+    var fileContainer = document.getElementById('fileContainer');
+
+    for (var i = 0; i < selectedFiles.length; i++) {
+      var fileBox = document.createElement('li');
+      fileBox.classList.add('file-box');
+      fileBox.dataset.index = i + 1;
+
+      var fileName = document.createElement('p');
+      fileName.classList.add('file-name');
+      fileName.textContent = selectedFiles[i].name;
+      
+      var deleteButton = document.createElement('span');
+      deleteButton.classList.add('delete-button');
+      deleteButton.textContent = '삭제';
+
+      deleteButton.addEventListener('click', function () {
+        fileBox.remove();
+      });
+
+      fileBox.appendChild(fileName);
+      fileBox.appendChild(deleteButton);
+      fileContainer.appendChild(fileBox);
     }
-  }
-  
+}
