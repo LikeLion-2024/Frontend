b3acab261e4ae7db126ddf41f382abc6694b5dfc
wonjeongjeong <wj3714@naver.com>
2024-03-10 19:03:50 +0900

    화면 왼쪽으로 치우친 현상 해결

--- js/homework.js
+++ js/homework.js
@@ -3,9 +3,9 @@ function handleFileSelection() {
     var fileContainer = document.getElementById('fileContainer');
 
     for (var i = 0; i < selectedFiles.length; i++) {
-      var fileBox = document.createElement('li');
-      fileBox.classList.add('file-box');
-      fileBox.dataset.index = i + 1;
+      var fileList = document.createElement('li');
+      fileList.classList.add('file-box');
+      fileList.dataset.index = i + 1;
 
       var fileName = document.createElement('p');
       fileName.classList.add('file-name');
@@ -16,11 +16,38 @@ function handleFileSelection() {
       deleteButton.textContent = '삭제';
 
       deleteButton.addEventListener('click', function () {
-        fileBox.remove();
+        fileList.remove();
       });
 
-      fileBox.appendChild(fileName);
-      fileBox.appendChild(deleteButton);
-      fileContainer.appendChild(fileBox);
+      fileList.appendChild(fileName);
+      fileList.appendChild(deleteButton);
+      fileContainer.appendChild(fileList);
     }
+}
+
+function submitForm() {
+  // 더미 데이터 생성
+  var submitterData = {
+      submitter: 'loopy'
+      // 다른 필요한 더미 데이터 추가 가능
+  };
+
+  // FormData 객체 생성
+  var formData = new FormData();
+
+  // 더미 데이터를 FormData에 추가
+  for (var key in submitterData) {
+      formData.append(key, submitterData[key]);
+  }
+
+  // 파일 input에서 선택한 파일들을 FormData에 추가
+  var fileInput = document.getElementById('fileInput');
+  for (var i = 0; i < fileInput.files.length; i++) {
+      formData.append('files[]', fileInput.files[i]);
+  }
+
+  // 콘솔에 FormData 출력
+  for (var pair of formData.entries()) {
+      console.log(pair[0] + ': ' + pair[1]);
+  }
 }
